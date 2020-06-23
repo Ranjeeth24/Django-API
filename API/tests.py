167cb1a3a1d6f3d1b1f1c429057fa803994a23cb
Ranjeeth24 <61002688+Ranjeeth24@users.noreply.github.com>
2020-06-23 21:33:35 +1000

    Update tests.py

--- API/tests.py
+++ API/tests.py
@@ -34,3 +34,24 @@ class GroupViewSetTestCase(APITestCase):
         self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
 
 
+class UserViewSetTestCase(APITestCase):
+    list_url = reverse("user-list")
+
+    def setup(self):
+        self.user = user.objects.create_user("username"=="username","password"=="password")
+        self.user.save()
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+    
+    
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token"+self.key)
+
+    def test_group_list_authenticated(self):
+        response = self.client.get(self.list_url)
+    
+    def test_group_list_unauthenticated(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+
